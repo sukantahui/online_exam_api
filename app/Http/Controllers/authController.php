5bc3aa8350ff4cf72807e22664c4446f0c2efa34
sukantahui <huisukanta77@gmail.com>
2023-07-29 21:33:13 +0530

    add register and login api

---
+++ app/Http/Controllers/authController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class authController extends Controller
+{
+    public function login(Request $request)
+    {
+        // Auth::attempt hashes the entered password using bcrypt
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid login details'], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+        ]);
+
+    }
+
+    public function register(Request $request)
+    {
+
+        $user =User::create(['name'=>$request['name']
+            ,'email'=>$request['email']
+            ,'password'=>$request['password']
+        ]);
+
+//        return response()->json(['success'=>1,'data'=>$user], 200,[],JSON_NUMERIC_CHECK);
+
+//        $token = $user->createToken('my-app-token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => '$token tt'
+        ];
+
+        return response($user, 201);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $guarded = ['id'];
     protected $fillable = [
         'name',
         'email',
@@ -41,4 +42,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function setPasswordAttribute($password)
+    {
+        if ( !empty($password) ) {
+            $this->attributes['password'] = bcrypt($password);
+        }
+    }
 }
